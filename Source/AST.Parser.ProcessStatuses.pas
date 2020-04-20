5ef9eeea36d64cb45bc3b735cb7daac1c8d29bfb
oleksandr.skliar <oleksandr.skliar@globallogic.com>
2020-04-20 20:14:05 +0300

    added new unit for process statuses

---
+++ Source/AST.Parser.ProcessStatuses.pas
@@ -0,0 +1,53 @@
+unit AST.Parser.ProcessStatuses;
+
+interface
+
+type
+
+  // Base AST process class. Should be expanded for derived parsing/compiling/build processes
+  TASTProcessStatus = class
+  public
+    class function Name: string; virtual; abstract;
+  end;
+
+  TASTProcessStatusClass = class of TASTProcessStatus;
+
+  TASTStatusParseBegin = class(TASTProcessStatus)
+  public
+    class function Name: string; override;
+  end;
+
+  TASTStatusParseSuccess = class(TASTProcessStatus)
+  public
+    class function Name: string; override;
+  end;
+
+  TASTStatusParseFail = class(TASTProcessStatus)
+  public
+    class function Name: string; override;
+  end;
+
+implementation
+
+{ TASTStatusParseBegin }
+
+class function TASTStatusParseBegin.Name: string;
+begin
+  Result := 'parse begin';
+end;
+
+{ TASTStatusParseSuccess }
+
+class function TASTStatusParseSuccess.Name: string;
+begin
+  Result := 'parse success';
+end;
+
+{ TASTStatusParseFail }
+
+class function TASTStatusParseFail.Name: string;
+begin
+  Result := 'parse fail';
+end;
+
+end.
